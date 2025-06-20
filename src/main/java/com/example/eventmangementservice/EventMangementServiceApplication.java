da30faf234c8a636daad5a4e85a1d0d3d8c18cc2
MeherChand <meher.chand@gojek.com>
2025-06-20 08:38:37 +0530

    added controllers,services,DTO,kafka setup

---
+++ src/main/java/com/example/eventmangementservice/EventMangementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventmangementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMangementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMangementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventmangementservice/EventMangementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmangementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMangementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
